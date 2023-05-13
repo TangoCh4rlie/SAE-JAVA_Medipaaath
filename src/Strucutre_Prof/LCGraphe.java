6d969cf5209bac6797a5d474042a7f9fdb5ef07a
Keap <haithemly69@gmail.com>
2023-05-13 12:58:06 +0200

    Ajout des fonction de chargement et du parcours Dijkstra

--- src/Strucutre_Prof/LCGraphe.java
+++ src/Strucutre_Prof/LCGraphe.java
@@ -1,3 +1,7 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
 class LCGraphe {
     public class MaillonGrapheSec {
         private double fiab;
@@ -46,19 +50,23 @@ class LCGraphe {
     public void addEdge(String o, String d, double fiab, double dist, double dur){
         MaillonGrapheSec nouv = new MaillonGrapheSec(fiab, dist, dur, d);
         MaillonGraphe tmp = this.premier;
-        while (!tmp.nom.equals(o)){
+        while (tmp != null && !tmp.nom.equals(o)){
             tmp = tmp.suiv;
         }
-        nouv.suiv = tmp.lVois;
-        tmp.lVois = nouv;
+        if (tmp != null) {
+            nouv.suiv = tmp.lVois;
+            tmp.lVois = nouv;
+        }
         
         MaillonGrapheSec nouv2 = new MaillonGrapheSec(fiab, dist, dur, o);
         tmp = this.premier;
-        while (!tmp.nom.equals(d)){
+        while (tmp != null && !tmp.nom.equals(d)){
             tmp = tmp.suiv;
         }
-        nouv2.suiv = tmp.lVois;
-        tmp.lVois = nouv2;
+        if (tmp != null) {
+            nouv2.suiv = tmp.lVois;
+            tmp.lVois = nouv2;
+        }
     }
 
     public String toString() {
@@ -81,7 +89,7 @@ class LCGraphe {
         String res = "";
         MaillonGraphe tmp = this.premier;
         while (tmp != null) {
-            if (tmp.type.equals("Bloc")) res+= tmp.nom + " - ";
+            if (tmp.type.equals("B")) res+= tmp.nom + " - ";
             tmp = tmp.suiv;
         }
         return res;
@@ -91,7 +99,7 @@ class LCGraphe {
         String res = "";
         MaillonGraphe tmp = this.premier;
         while (tmp != null) {
-            if (tmp.type.equals("Maternité")) res+= tmp.nom + " - ";
+            if (tmp.type.equals("M")) res+= tmp.nom + " - ";
             tmp = tmp.suiv;
         }
         return res;
@@ -101,7 +109,7 @@ class LCGraphe {
         String res = "";
         MaillonGraphe tmp = this.premier;
         while (tmp != null) {
-            if (tmp.type.equals("Nutrition")) res+= tmp.nom + " - ";
+            if (tmp.type.equals("N")) res+= tmp.nom + " - ";
             tmp = tmp.suiv;
         }
         return res;
@@ -136,5 +144,188 @@ class LCGraphe {
             tmp2 = tmp2.suiv;
         }
         return res;
+    }
+
+
+    public MaillonGraphe recherchenom(String nom){
+        MaillonGraphe tmp = this.premier;
+        while (tmp != null && !tmp.nom.equals(nom)) {
+            tmp = tmp.suiv;
+        }
+        return tmp;
+    }
+
+    public void removeDuplicateEdges() {
+        MaillonGraphe current = premier;
+        while (current != null) {
+            MaillonGrapheSec currentEdge = current.lVois;
+            while (currentEdge != null) {
+                MaillonGrapheSec previous = currentEdge;
+                MaillonGrapheSec next = currentEdge.suiv;
+                while (next != null) {
+                    if (next.dest.equals(currentEdge.dest)) {
+                        previous.suiv = next.suiv;
+                        next = previous.suiv;
+                    } else {
+                        previous = next;
+                        next = next.suiv;
+                    }
+                }
+                currentEdge = currentEdge.suiv;
+            }
+            current = current.suiv;
+        }
+    }
+
+    public LCGraphe charg() throws IOException{
+        File fr = new File("C:/Users/Haithem/Desktop/liste-successeurs.csv");
+        Scanner sc = new Scanner(fr);
+        int line = 0;
+        while (sc.hasNext())
+            {   
+                String s = sc.nextLine();
+                if (line == 0) {
+                    s = s.substring(3);
+                    line++;
+                }
+                String[] parts = s.split(";");
+                String nom = parts[0];
+                String type = null;
+
+                this.addMain(nom,type);
+                
+                
+        }
+        sc.close();    
+        File fr2 = new File("C:/Users/Haithem/Desktop/liste-adjacence-jeuEssai.csv");
+        Scanner sc2 = new Scanner(fr2);
+        while (sc2.hasNext())
+            {
+                String s = sc2.nextLine();
+                String[] parts = s.split(";");
+                String ori = parts[0];
+                MaillonGraphe cible = recherchenom(ori);
+                cible.type = parts[1];
+                int i = 2;
+                while (i < (parts.length)-2) {
+                    if (parts[i].equals("0")){
+                        i++;
+                    }
+                    else{
+                        String[] parts2 = parts[i].split(",");
+                        double fiab = Double.parseDouble(parts2[0]);
+                        double dist = Double.parseDouble(parts2[1]);
+                        double dur = Double.parseDouble(parts2[2]);
+
+                        Scanner sc3 = new Scanner(fr2);
+                        while(sc3.hasNext() ){
+                            String s2 = sc3.nextLine();
+                            String[] parts3 =  s2.split(";");
+                            if(!parts3[0].equals(ori)){
+                                if(parts3[i].equals(parts[i])){
+                                    String dest = parts3[0];
+                                    this.addEdge(ori,dest,fiab,dist,dur);
+                                }
+                            }
+                        }
+                        sc3.close();
+                        i++;
+                    }
+                }
+        }
+        sc2.close();
+        this.removeDuplicateEdges();
+        return this;
+    }
+
+
+    
+    public void resetListed(){
+        MaillonGraphe tmp = this.premier;
+        while (tmp != null) {
+            tmp.listed = false;
+            tmp = tmp.suiv;
+        }
+    }
+
+    public void dijkstra(String start, String end) {
+        Map<String, Double> distances = new HashMap<>();
+        MaillonGraphe current = premier;
+        while (current != null) {
+            distances.put(current.nom, Double.POSITIVE_INFINITY);
+            current = current.suiv;
+        }
+        distances.put(start, 0.0);
+        PriorityQueue<MaillonGraphe> queue = new PriorityQueue<>(Comparator.comparingDouble(o -> distances.get(o.nom)));
+        current = premier;
+        while (current != null) {
+            if (current.nom.equals(start)) {
+                queue.add(current);
+            }
+            current = current.suiv;
+        }
+
+        while (!queue.isEmpty()) {
+            current = queue.poll();
+            current.listed = true;
+            MaillonGrapheSec edge = current.lVois;
+            while (edge != null) {
+                MaillonGraphe next = premier;
+                while (next != null && !next.nom.equals(edge.dest)) {
+                    next = next.suiv;
+                }
+                if (next != null && !next.listed) {
+                    double newDist = distances.get(current.nom) + edge.dur;
+                    if (newDist < distances.get(next.nom)) {
+                        distances.put(next.nom, newDist);
+                        queue.remove(next);
+                        queue.add(next);
+                    }
+                }
+                edge = edge.suiv;
+            }
+        }
+        List<String> path = new ArrayList<>();
+        current = premier;
+        while (current != null && !current.nom.equals(end)) {
+            current = current.suiv;
+        }
+        if (current == null || distances.get(current.nom) == Double.POSITIVE_INFINITY) {
+            System.out.println("No path found.");
+            return;
+        }
+        path.add(current.nom);
+        while (!current.nom.equals(start)) {
+            MaillonGrapheSec edge = current.lVois;
+            while (edge != null) {
+                MaillonGraphe next = premier;
+                while (next != null && !next.nom.equals(edge.dest)) {
+                    next = next.suiv;
+                }
+                if (next != null && distances.get(next.nom) + edge.dur == distances.get(current.nom)) {
+                    path.add(next.nom);
+                    current = next;
+                    break;
+                }
+                edge = edge.suiv;
+            }
+        }
+        Collections.reverse(path);
+        System.out.print("Plus cours chemin: " + path.get(0));
+        for (int i = 1; i < path.size(); i++) {
+            System.out.print(" -> " + path.get(i));
+        }
+        System.out.println();
+        System.out.println("Distance total : " + distances.get(end));
+    }
+    
+    public static void main(String[] args) throws IOException{
+        LCGraphe g = new LCGraphe();
+        g.charg();
+        System.out.println(g.toString());
+        System.out.println("////");
+        System.out.println(g.printMaternite());
+        System.out.println("////");
+        g.dijkstra("S1", "S5");
     }
 }
