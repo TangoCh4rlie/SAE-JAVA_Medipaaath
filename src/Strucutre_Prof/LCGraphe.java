33c388dd5bb0341bf1553cd9c25c33add04aa927
Keap <haithemly69@gmail.com>
2023-05-19 22:04:34 +0200

    Ajout de la Java Doc + Commentaire des parties complexes du code  - close #4

--- src/Strucutre_Prof/LCGraphe.java
+++ src/Strucutre_Prof/LCGraphe.java
@@ -1,5 +1,3 @@
-
-
 import java.io.*;
 import java.util.*;
 
@@ -43,12 +41,28 @@ class LCGraphe {
     }
     
     public void addMain(String ori, String t){
+        /*
+         * @autor : Haithem
+         * @description : ajoute un sommet au graphe a partir de son nom et son type
+         * a la liste chainee de sommets
+         * @param : String ori, String t
+         * @return : void
+         */
+
         MaillonGraphe nouv = new MaillonGraphe(ori,t);
         nouv.suiv= this.premier;
         this.premier = nouv;
     }
     
     public void addEdge(String o, String d, double fiab, double dist, double dur){
+
+        /*
+         * @autor : Haithem
+         * @description : ajoute une arete au graphe a partir de son origine, sa destination,
+         * sa fiabilite, sa distance et sa duree
+         * @param : String o, String d, double fiab, double dist, double dur
+         * @return : void
+         */
         MaillonGrapheSec nouv = new MaillonGrapheSec(fiab, dist, dur, d);
         MaillonGraphe tmp = this.premier;
         while (tmp != null && !tmp.nom.equals(o)){
@@ -71,6 +85,12 @@ class LCGraphe {
     }
 
     public String toString() {
+        /*
+         * @autor : Haithem
+         * @description : affiche le graphe
+         * @param : void
+         * @return : String
+         */
         String s = "";
         MaillonGraphe tmp = this.premier;
         while (tmp != null) {
@@ -87,6 +107,12 @@ class LCGraphe {
     }
 
     public String printBlock(){
+        /*
+         * @autor : Haithem
+         * @description : affiche les sommets de type Block opératoire "B"
+         * @param : void
+         * @return : String
+         */
         String res = "";
         MaillonGraphe tmp = this.premier;
         while (tmp != null) {
@@ -97,6 +123,12 @@ class LCGraphe {
     }
 
     public String printMaternite(){
+        /*
+         * @autor : Haithem
+         * @description : affiche les sommets de type Maternité "M"
+         * @param : void
+         * @return : String
+         */
         String res = "";
         MaillonGraphe tmp = this.premier;
         while (tmp != null) {
@@ -107,6 +139,12 @@ class LCGraphe {
     }
 
     public String printNutrition(){
+        /*
+         * @autor : Haithem
+         * @description : affiche les sommets de type Nutrition "N"
+         * @param : void
+         * @return : String
+         */
         String res = "";
         MaillonGraphe tmp = this.premier;
         while (tmp != null) {
@@ -116,23 +154,13 @@ class LCGraphe {
         return res;
     }
 
-    public String printWays(){
-        String res = "";
-        MaillonGraphe tmp = this.premier;
-        while (tmp != null) {
-            res+= ("Trajet de "+tmp.nom + " a : ");
-            MaillonGrapheSec tmp2 = tmp.lVois;
-            while (tmp2 != null) {
-                res += tmp2.dest + " (" + tmp2.fiab + ", " + tmp2.dist + ", " + tmp2.dur + ") -> ";
-                tmp2 = tmp2.suiv;
-            }
-            res = res + '\n';
-            tmp = tmp.suiv;
-        }
-        return res;
-    }
-
     public String oneDistNeighbors(String disp){
+        /*
+         * @autor : Haithem
+         * @description : affiche les voisins a 1 distance d'un sommet
+         * @param : String disp
+         * @return : String
+         */
         String res = "";
         MaillonGraphe tmp = this.premier;
         while (!tmp.nom.equals(disp)) {
@@ -141,7 +169,9 @@ class LCGraphe {
         MaillonGrapheSec tmp2 = tmp.lVois;
 
         while (tmp2 != null) {
-            if (tmp2.dist == 1) res += tmp2.dest + " - ";
+            if (tmp2.dist == 1){
+                res += tmp2.dest + " - ";
+            }
             tmp2 = tmp2.suiv;
         }
         return res;
@@ -149,14 +179,27 @@ class LCGraphe {
 
 
     public MaillonGraphe recherchenom(String nom){
+        /*
+         * @autor : Haithem
+         * @description : recherche un sommet a partir de son nom
+         * @param : String nom
+         * @return : MaillonGraphe
+         */
         MaillonGraphe tmp = this.premier;
         while (tmp != null && tmp.nom.equals(nom)) {
+            //Parcours de la liste chainee de sommets
             tmp = tmp.suiv;
         }
         return tmp;
     }
 
     public void removeDuplicateEdges() {
+        /*
+         * @autor : Haithem
+         * @description : supprime les arretes dupliquées dans le graphe lors du chargement
+         * @param : void
+         * @return : void
+         */
         MaillonGraphe current = premier;
         while (current != null) {
             MaillonGrapheSec currentEdge = current.lVois;
@@ -165,9 +208,11 @@ class LCGraphe {
                 MaillonGrapheSec next = currentEdge.suiv;
                 while (next != null) {
                     if (next.dest.equals(currentEdge.dest)) {
+                        // Suppression de l'arrete
                         previous.suiv = next.suiv;
                         next = previous.suiv;
                     } else {
+                        // Passage a l'arrete suivante
                         previous = next;
                         next = next.suiv;
                     }
@@ -179,10 +224,17 @@ class LCGraphe {
     }
 
     public LCGraphe charg() throws IOException{
+        /*
+         * @autor : Haithem
+         * @description : charge le graphe a partir d'un fichier csv
+         * @param : void
+         * @return : LCGraphe
+         */
         File fr2 = new File("C:/Users/Haithem/Desktop/liste-adjacence-jeuEssai.csv");
         Scanner sc2 = new Scanner(fr2);
         while (sc2.hasNext())
             {
+                //Chargement des sommets
                 String s = sc2.nextLine();
                 String[] parts = s.split(";");
                 String ori = parts[0];
@@ -194,17 +246,19 @@ class LCGraphe {
                     }
                     else{
                         String[] parts2 = parts[i].split(",");
+                        //Chargement des arretes
                         double fiab = Double.parseDouble(parts2[0]);
                         double dist = Double.parseDouble(parts2[1]);
                         double dur = Double.parseDouble(parts2[2]);
-
                         Scanner sc3 = new Scanner(fr2);
                         while(sc3.hasNext() ){
+                            //Parcours du fichier pour trouver les sommets de destination
                             String s2 = sc3.nextLine();
                             String[] parts3 =  s2.split(";");
                             if(!parts3[0].equals(ori)){
                                 if(parts3[i].equals(parts[i])){
                                     String dest = parts3[0];
+                                    //Ajout de l'arrete
                                     this.addEdge(ori,dest,fiab,dist,dur);
                                 }
                             }
@@ -215,11 +269,18 @@ class LCGraphe {
                 }
         }
         sc2.close();
+        //Suppression des arretes dupliquées
         this.removeDuplicateEdges();
         return this;
     }
 
     public void resetListed(){
+        /*
+         * @autor : Haithem
+         * @description : remet a false la valeur listed de tous les sommets
+         * @param : void
+         * @return : void
+         */
         MaillonGraphe tmp = this.premier;
         while (tmp != null) {
             tmp.listed = false;
@@ -228,9 +289,17 @@ class LCGraphe {
     }
 
     public void dijkstra(String start, String end) {
+        /*
+         * @autor : Haithem
+         * @description : applique l'algorithme de dijkstra pour trouver le plus court chemin
+         * entre deux sommets
+         * @param : String start, String end
+         * @return : void
+         */
         Map<String, Double> distances = new HashMap<>();
         MaillonGraphe current = premier;
         while (current != null) {
+            //Initialisation des distances a l'infini
             distances.put(current.nom, Double.POSITIVE_INFINITY);
             current = current.suiv;
         }
@@ -238,6 +307,7 @@ class LCGraphe {
         PriorityQueue<MaillonGraphe> queue = new PriorityQueue<>(Comparator.comparingDouble(o -> distances.get(o.nom)));
         current = premier;
         while (current != null) {
+            //Ajout des sommets a la file de priorite
             if (current.nom.equals(start)) {
                 queue.add(current);
             }
@@ -252,11 +322,14 @@ class LCGraphe {
             while (edge != null) {
                 MaillonGraphe next = premier;
                 while (next != null && !next.nom.equals(edge.dest)) {
+                    //Parcours de la liste chainee de sommets
                     next = next.suiv;
                 }
                 if (next != null && !next.listed) {
+                    //Si le sommet n'est pas encore visité
                     double newDist = distances.get(current.nom) + edge.dur;
                     if (newDist < distances.get(next.nom)) {
+                        //Mise a jour de la distance
                         distances.put(next.nom, newDist);
                         queue.remove(next);
                         queue.add(next);
@@ -268,14 +341,17 @@ class LCGraphe {
         List<String> path = new ArrayList<>();
         current = premier;
         while (current != null && !current.nom.equals(end)) {
+            //Parcours de la liste chainee de sommets
             current = current.suiv;
         }
         if (current == null || distances.get(current.nom) == Double.POSITIVE_INFINITY) {
+            //Si le sommet n'est pas atteignable (graphe non connexe)
             System.out.println("No path found.");
             return;
         }
         path.add(current.nom);
         while (!current.nom.equals(start)) {
+            //Parcours du chemin
             MaillonGrapheSec edge = current.lVois;
             while (edge != null) {
                 MaillonGraphe next = premier;
@@ -283,6 +359,7 @@ class LCGraphe {
                     next = next.suiv;
                 }
                 if (next != null && distances.get(next.nom) + edge.dur == distances.get(current.nom)) {
+                    //Si la distance est la bonne
                     path.add(next.nom);
                     current = next;
                     break;
@@ -290,6 +367,7 @@ class LCGraphe {
                 edge = edge.suiv;
             }
         }
+        //Affichage du chemin
         Collections.reverse(path);
         System.out.print("Plus cours chemin: " + path.get(0));
         for (int i = 1; i < path.size(); i++) {
@@ -301,11 +379,18 @@ class LCGraphe {
     
 
     public void countEdges(){
+        /*
+         * @autor : Haithem
+         * @description : compte le nombre d'arretes dans le graphe
+         * @param : void
+         * @return : void
+         */
         MaillonGraphe tmp = this.premier;
         int count = 0;
         while (tmp != null) {
             MaillonGrapheSec tmp2 = tmp.lVois;
             while (tmp2 != null) {
+                // ajout de 1 a chaque arrete
                 count++;
                 tmp2 = tmp2.suiv;
             }
